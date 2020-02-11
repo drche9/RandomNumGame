bb47fb3c3e5c70e0b72fff2210cf1542c84be5be
drche9 <58885721+drche9@users.noreply.github.com>
2020-02-11 15:11:22 +0900

    DiceGame(완)

---
+++ main.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main() {
+	int ran;
+	int answer = 0;
+
+	printf("1. start , 2. stop ==> ");
+	scanf_s("%d", &answer);
+	
+	if (answer == 1) {
+		printf("start \n");
+	}
+	else if (answer == 2) {
+		printf("exit");
+		exit(0);
+	}
+	printf("\n \n");
+
+	for (answer; answer == 1;) {
+		srand(time(NULL));
+		ran = (rand() % 6) + 1;
+		printf("\n dice ==> %d \n \n", ran);
+		printf("restart ==> 1 \n");
+		printf("end ==> 2 \n");
+		scanf_s("%d", &answer);
+	}
+	return 0;
+}
